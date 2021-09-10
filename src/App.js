219053835dacff9d37f65cb6e06151210e59ef92
Ben Abbott <babbott85@gmail.com>
2021-09-09 20:00:47 -0600

    footer component added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header'
-
+import Footer from './components/footer'
 function App() {
   return (
     <div className="App">
---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function footer (){
+    return (
+        <>
+        <a href="https://www.linkedin.com/in/ben-abbott-51394421/"><i class="fab fa-linkedin fa-4x linkedin" ></i></a>
+      
+      <a href="https://github.com/benabbott85"><i class="fab fa-github fa-4x github"></i></a>
+      </>
+    )
+}
+
+export default footer
