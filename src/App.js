f8397a495c97d799d1e367d106caedaae854f7d7
Ben Abbott <babbott85@gmail.com>
2021-09-14 21:09:36 -0600

    light/dark mode figure out

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import React, {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header'
 import Footer from './components/footer'
 import Contact from './components/contact'
-import Portfolio from './components/portfolio'
+import Portfolio from './components/portfolio';
+import Color from './components/darklight'
+
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyles } from "./components/modetheme";
+
+const StyleApp = styled.div`
+  color: ${(props) => props.theme.fontColor};
+`;
 function App() {
+
+  const [theme, setTheme] = useState("light");
+
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
   return (
     <div className="App">
+      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <StyleApp>
+      <button onClick={() => themeToggler()}>Light Mode/Dark Mode</button>
+      {/* <Color> */}
       {/* <Header/> */}
 <h1>Ben Abbott</h1>
 <h2>Full Stack Software Engineer</h2>
@@ -22,6 +43,10 @@ function App() {
 <br/>
 <br/>
 <Footer/>
+{/* </Color> */}
+
+      </StyleApp>
+    </ThemeProvider>
     </div>
   );
 }
