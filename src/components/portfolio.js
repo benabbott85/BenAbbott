ac9d433aef3bade003ee827522c3f87bd6cf9de3
Ben Abbott <babbott85@gmail.com>
2021-11-12 09:33:21 -0700

    more media queries

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -12,7 +12,7 @@ function Portfolio(){
 <Row>
 
     <Card className='ecard'>
-    <Col md={4}>
+    <Col md={4} className ='cards'>
         <CardTitle className='etitle'>East River Homes</CardTitle>
         <CardImg className='eriver' src={River} alt='east river'/>
         {/* <a href="https://eastriver-contracting.com/" target='_blank'><Button className="eastbtn">View More</Button></a> */}
@@ -26,7 +26,7 @@ function Portfolio(){
 
     
     <Card className="ruckcard">
-    <Col md={4}>
+    <Col md={4} className ='cards'>
         <CardTitle className='rtitle'>Ruck Roofing</CardTitle>
         <CardImg className='ruckimg' src={Ruck} alt='east river'/>
         {/* <a href="https://ruckroofing.com/" target='_blank'><Button className="ruckbtn">View More</Button></a> */}
@@ -38,7 +38,7 @@ function Portfolio(){
     <br/>
     
     <Card className="vcard">
-    <Col md={4}>
+    <Col md={4} className ='cards'>
         <CardTitle className='vtitle'>VoteFromHome</CardTitle>
         <CardImg className='vhome' src='https://ca-times.brightspotcdn.com/dims4/default/04e367d/2147483647/strip/true/crop/2048x1152+0+0/resize/1486x836!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F55%2F17%2F4eacf672c654bed1adb04ce34a85%2Fla-pol-us-vote-california-20160702-photo' alt='east river'/>
         {/* <a href="https://github.com/benabbott85/coronavote" target='_blank'><Button className="eastbtn">View More</Button></a> */}
